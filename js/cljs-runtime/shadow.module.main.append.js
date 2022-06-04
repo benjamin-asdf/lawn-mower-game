a4e37101599c57e4c7ddbcfaa207aff7a141a244
Benj <Benjamin.Schwerdtner@gmail.com>
2022-06-04 15:59:39 +0200

    deploy

--- js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { benjamin.lawnmower.core.init(); } catch (e) { console.error("An error occurred when calling (benjamin.lawnmower.core/init)"); throw(e); }
